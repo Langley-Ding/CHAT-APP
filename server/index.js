1f101bdbc19f72aae901594734f76aaf7561825f
Langley-Ding <374376995@qq.com>
2020-11-15 22:53:40 +0800

    52:00

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+
 
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
-const { use } = require('./router');
+// const { use } = require('./router');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,10 +15,17 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!!');
 
+    socket.on('join', ({name, room}, callback) => {
+        console.log(name, room);
+        
+        
+    })
     socket.on('disconnect', () => {
         console.log('User had left!!!!');
     })
-})
+});
+
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
